416c0d4552797c5bd057829690262843ad6846bf
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-14 20:13:40 +0300

    chore: Add ASGI and WSGI configuration files for Django deployment

---
+++ Rentoo/settings.py
@@ -0,0 +1,135 @@
+from datetime import timedelta
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = "django-insecure-!-73#qx8msma^+az0yi$tz%sha-n4#=)zw2cb5-l5&7!xo-n91"
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+AUTH_USER_MODEL = "authentication.CustomUser"
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "drf_yasg",
+    "rest_framework_simplejwt",
+    "rest_framework.authtoken",
+    "corsheaders",
+    # local apps
+    "tenants",
+    "authentication.apps.AuthenticationConfig",
+    "agents.apps.AgentsConfig",
+    "properties.apps.PropertiesConfig",
+    "communication.apps.CommunicationConfig",
+    "maintenance.apps.MaintenanceConfig",
+    "landlords.apps.LandlordsConfig",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOW_HEADERS = [
+    "content-type",
+    "authorization",
+    "x-csrftoken",
+]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:53946",
+    "http://localhost:3000",
+]
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "OPTIONS",
+    "DELETE",
+    "PUT",
+    "PATCH",
+]
+
+
+ROOT_URLCONF = "Rentoo.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "Rentoo.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Africa/Nairobi"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = "static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# rest_framework settings
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",  # Add this line
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
+    "SLIDING_TOKEN_LIFETIME": timedelta(days=30),
+    "SLIDING_TOKEN_REFRESH_LIFETIME_LATE_USER": timedelta(days=1),
+}
---
+++ Rentoo/urls.py
@@ -0,0 +1,47 @@
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Rentoo API",
+        default_version="v1",
+        description="API for Rentoo",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="nyamburanjorogejames@students.uonbi.ac.ke"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("auth/", include("authentication.urls")),
+    # properties
+    path("properties/", include("properties.urls")),
+    # tenants
+    path("tenants/", include("tenants.urls")),
+    # landlords
+    path("landlords/", include("landlords.urls")),
+    # agents
+    path("agents/", include("agents.urls")),
+    # communication
+    path("communication/", include("communication.urls")),
+    # maintenance
+    path("maintenance/", include("maintenance.urls")),
+    # swagger docs
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    # jwt token
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
