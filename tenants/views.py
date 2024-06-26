b8b4733eaac215d7186bf66a85c2b94121b5d76b
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:12:38 +0300

    refactor: Reorganize imports in tenants/views.py
    Reorganize the imports in tenants/views.py to group related imports together and improve code readability.

--- tenants/views.py
+++ tenants/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render
-from .serializers import TenantSerializer, LeaseSerializer, PaymentSerializer
-from .models import Tenant, Lease, Payment
 from rest_framework import viewsets
 
+from .models import Lease, Payment
+from .serializers import LeaseSerializer, PaymentSerializer
 
-class TenantViewSet(viewsets.ModelViewSet):
-    queryset = Tenant.objects.all()
-    serializer_class = TenantSerializer
+
 class LeaseViewSet(viewsets.ModelViewSet):
     queryset = Lease.objects.all()
     serializer_class = LeaseSerializer
+
+
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
-
