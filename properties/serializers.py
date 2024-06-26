7aed5e06fdf8de0c976094e29ad2c4ff0135f8ea
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:12:02 +0300

    refactor: Update serializers.py to use EntityPhoto instead of PropertyPhoto and HousePhoto
    Update the serializers.py file in the properties app to use the EntityPhoto model instead of the PropertyPhoto and HousePhoto models. This change improves clarity and consistency in the codebase.

--- properties/serializers.py
+++ properties/serializers.py
@@ -1,26 +1,20 @@
 from rest_framework import serializers
-from .models import Property, House, PropertyPhoto, HousePhoto
+from .models import Property, House, EntityPhoto
 
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields = '__all__'
+        fields = "__all__"
 
 
 class HouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = House
-        fields = '__all__'
+        fields = "__all__"
 
 
-class PropertyPhotoSerializer(serializers.ModelSerializer):
+class EntityPhotoSerializer(serializers.ModelSerializer):
     class Meta:
-        model = PropertyPhoto
-        fields = '__all__'
-
-
-class HousePhotoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = HousePhoto
-        fields = '__all__'
+        model = EntityPhoto
+        fields = "__all__"
