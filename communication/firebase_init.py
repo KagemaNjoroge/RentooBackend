0fc211e4b3a56f9a5e3c8d03e5ca67e86575cd9e
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:09:08 +0300

    refactor: Update Firebase service account key path

--- communication/firebase_init.py
+++ communication/firebase_init.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Path to your Firebase service account credentials JSON file
-FIREBASE_SERVICE_ACCOUNT_KEY = "C:\\Users\\kaman\\Desktop\\Rentoo\\communication\\rentoo-rentals-management-firebase-adminsdk-l8c06-6a5314d56c.json"
+FIREBASE_SERVICE_ACCOUNT_KEY = "C:\\Users\\reece\\OneDrive\\Desktop\\RentooBackend\\google-services.json"
 
 # Initialize Firebase Admin SDK
 cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_KEY)
