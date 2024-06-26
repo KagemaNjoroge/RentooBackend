45c18fa10a38c1222651eb22d5cabdbe58abc029
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:09:47 +0300

    refactor: Update LandlordAdmin in landlords/admin.py
    Simplify the LandlordAdmin class in landlords/admin.py by removing the PropertyInline class and its related import. This change improves the readability and maintainability of the code.

--- landlords/admin.py
+++ landlords/admin.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
+
 from .models import Landlord
-from properties.models import Property
-
-
-class PropertyInline(admin.TabularInline):
-    model = Property
-    extra = 1
 
 
 @admin.register(Landlord)
 class LandlordAdmin(admin.ModelAdmin):
-    list_display = ('user',)
-    search_fields = ('user__username', 'user__first_name', 'user__last_name', 'user__email')
-    inlines = [PropertyInline]
+    list_display = ("user",)
+    search_fields = (
+        "user__username",
+        "user__first_name",
+        "user__last_name",
+        "user__email",
+    )
