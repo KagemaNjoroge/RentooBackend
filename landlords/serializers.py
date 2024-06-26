978c448f6332bfd238229a31cc7a80d574f90c6f
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:09:59 +0300

    refactor: Remove LandlordSerializer from landlords/serializers.py
    Simplify the code by removing the unused LandlordSerializer class from landlords/serializers.py. This improves code readability and maintainability.

--- landlords/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Landlord
-
-
-class LandlordSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Landlord
-        fields = '__all__'
