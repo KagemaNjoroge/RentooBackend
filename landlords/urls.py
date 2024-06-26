481a4d8c4db0786f0ca18a33956ff5be739556f4
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:10:12 +0300

    refactor: Update landlords/urls.py to remove unused code
    Remove the unused code for registering the LandlordViewSet in landlords/urls.py. This change improves code cleanliness and maintainability.

--- landlords/urls.py
+++ landlords/urls.py
@@ -1,7 +1,1 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import LandlordViewSet
-
-router = DefaultRouter()
-router.register(r"", LandlordViewSet, basename="landlords")
-urlpatterns = router.urls
+urlpatterns = []
