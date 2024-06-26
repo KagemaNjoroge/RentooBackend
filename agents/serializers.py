1d5b1cd081e7d40cd492899b685f67a7a33dbff1
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:07:46 +0300

    refactor: Remove unused AgentSerializer from agents/serializers.py

--- agents/serializers.py
+++
@@ -1,8 +0,0 @@
-from .models import Agent
-from rest_framework import serializers
-
-
-class AgentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Agent
-        fields = '__all__'
