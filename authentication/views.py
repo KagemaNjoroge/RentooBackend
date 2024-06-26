e3184697e35422e1bf4fabe06585f5363a917d2b
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:08:49 +0300

    refactor: Override perform_create method in UserViewSet to set user password

--- authentication/views.py
+++ authentication/views.py
@@ -7,3 +7,9 @@ from .serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+
+    # override to  also create a password for the user
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
