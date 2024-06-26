11c91179221c0007e99c546a6ba4d170debc7f26
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:10:31 +0300

    refactor: Remove unused LandlordViewSet from landlords/views.py

--- landlords/views.py
+++
@@ -1,9 +0,0 @@
-from .models import Landlord
-from .serializers import LandlordSerializer
-from rest_framework import viewsets
-
-
-class LandlordViewSet(viewsets.ModelViewSet):
-    queryset = Landlord.objects.all()
-    serializer_class = LandlordSerializer
-
