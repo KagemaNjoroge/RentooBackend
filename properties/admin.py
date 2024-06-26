16fa116d3a5fb8bcb0c5830804c9026d9a03ca9e
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:11:32 +0300

    refactor: Update admin.py to use EntityPhoto instead of PropertyPhoto and HousePhoto
    Update the admin.py file in the properties app to use the EntityPhoto model instead of the PropertyPhoto and HousePhoto models. This change improves clarity and consistency in the codebase.

--- properties/admin.py
+++ properties/admin.py
@@ -1,46 +1,36 @@
 from django.contrib import admin
-from .models import Property, House, PropertyPhoto, HousePhoto
 
-admin.site.site_header = 'Rentoo Property Management System'
-admin.site.site_title = 'Rentoo PMS'
-admin.site.index_title = 'Welcome to Rentoo PMS'
+from .models import Property, House, EntityPhoto
 
-
-class PropertyPhotoInline(admin.TabularInline):
-    model = PropertyPhoto
-    extra = 1
-
-
-class HousePhotoInline(admin.TabularInline):
-    model = HousePhoto
-    extra = 1
+admin.site.site_header = "Rentoo Property Management System"
+admin.site.site_title = "Rentoo PMS"
+admin.site.index_title = "Welcome to Rentoo PMS"
 
 
 @admin.register(Property)
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'address', 'created_at', 'updated_at')
-    search_fields = ('name', 'address')
-    list_filter = ('created_at', 'updated_at')
-    inlines = [PropertyPhotoInline]
+    list_display = ("name", "address", "created_at", "updated_at")
+    search_fields = ("name", "address")
+    list_filter = ("created_at", "updated_at")
 
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    list_display = ('property', 'house_number', 'number_of_rooms', 'is_occupied', 'rent', 'created_at', 'updated_at')
-    search_fields = ('property__name', 'house_number')
-    list_filter = ('is_occupied', 'created_at', 'updated_at')
-    inlines = [HousePhotoInline]
+    list_display = (
+        "property",
+        "house_number",
+        "number_of_rooms",
+        "is_occupied",
+        "rent",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = ("property__name", "house_number")
+    list_filter = ("is_occupied", "created_at", "updated_at")
 
 
-@admin.register(PropertyPhoto)
+@admin.register(EntityPhoto)
 class PropertyPhotoAdmin(admin.ModelAdmin):
-    list_display = ('property', 'image', 'caption', 'uploaded_at')
-    search_fields = ('property__name', 'caption')
-    list_filter = ('uploaded_at',)
-
-
-@admin.register(HousePhoto)
-class HousePhotoAdmin(admin.ModelAdmin):
-    list_display = ('house', 'image', 'caption', 'uploaded_at')
-    search_fields = ('house__house_number', 'caption')
-    list_filter = ('uploaded_at',)
+    list_display = ("image", "caption", "uploaded_at")
+    search_fields = ("caption",)
+    list_filter = ("uploaded_at",)
