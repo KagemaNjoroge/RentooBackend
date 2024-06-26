fccbf437b96248cc9e5df5bac1a7b52cd2d9d51a
Kagema Njoroge <reecejames934@gmail.com>
2024-06-26 17:08:41 +0300

    refactor: Update UserSerializer to include all fields and set read-only permissions

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
-
 from .models import CustomUser
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ("id", "username", "email", "phone_number", "profile_picture")
-        extra_kwargs = {"password": {"write_only": True}}
+        fields = "__all__"
+        extra_kwargs = {
+            "password": {"write_only": True},
+            "is_active": {"read_only": True},
+            "is_supeuser": {"read_only": True},
+            "is_staff": {"read_only": True},
+            "is_superuser": {"read_only": True},
+            "last_login": {"read_only": True},
+            "date_joined": {"read_only": True},
+            "groups": {"write_only": True},
+            "user_permissions": {"write_only": True},
+        }
