e4cb8eb3a81dd465615f98911f36cd0cf1b59a4d
Kagema Njoroge <reecejames934@gmail.com>
2024-06-27 20:44:06 +0300

    Create agents model

--- agents/models.py
+++ agents/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Agent(models.Model):
-    user = models.OneToOneField('authentication.CustomUser', on_delete=models.CASCADE, related_name='agent')
+    user = models.OneToOneField(
+        "authentication.CustomUser", on_delete=models.CASCADE, related_name="agent"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -11,6 +13,6 @@ class Agent(models.Model):
         return self.user.username
 
     class Meta:
-        verbose_name = 'Agent'
-        verbose_name_plural = 'Agents'
-        ordering = ['user__username']
+        verbose_name = "Agent"
+        verbose_name_plural = "Agents"
+        ordering = ["user__username"]
