22a784f0587186ff7e28fedfec54b1f1dcde9657
Kagema Njoroge <reecejames934@gmail.com>
2024-06-27 20:12:09 +0300

    Remove unused import Add swagger endpoint description

--- properties/views.py
+++ properties/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from .serializers import (
     EntityPhotoSerializer,
     PropertySerializer,
@@ -6,11 +5,21 @@ from .serializers import (
 )
 from .models import Property, House, EntityPhoto
 from rest_framework import viewsets
+from drf_yasg.utils import swagger_auto_schema
+import time
 
 
 class PropertyViewSet(viewsets.ModelViewSet):
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
+
+    @swagger_auto_schema(
+        operation_summary="Get all properties",
+        operation_description="Get all properties from the database",
+    )
+    def list(self, request, *args, **kwargs):
+
+        return super().list(request, *args, **kwargs)
 
 
 class HouseViewSet(viewsets.ModelViewSet):
