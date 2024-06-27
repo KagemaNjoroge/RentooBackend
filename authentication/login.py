36162175aa6bdb02c78658db93250975aaabfe15
Kagema Njoroge <reecejames934@gmail.com>
2024-06-27 20:14:41 +0300

    Replace class based login view with a functional one for more control.

--- authentication/login.py
+++ authentication/login.py
@@ -1,64 +1,64 @@
 from django.contrib.auth import authenticate
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from drf_yasg.utils import swagger_auto_schema
+from .serializers import LoginSerializer
+from rest_framework.decorators import api_view
+from drf_yasg import openapi
 
-from agents.models import Agent
-from landlords.models import Landlord
-from tenants.models import Tenant
 
+@swagger_auto_schema(
+    method="POST",
+    request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT,
+        required=["username", "password"],
+        properties={
+            "username": openapi.Schema(type=openapi.TYPE_STRING),
+            "password": openapi.Schema(
+                type=openapi.TYPE_STRING, format=openapi.FORMAT_PASSWORD
+            ),
+        },
+    ),
+    operation_description="Login a user",
+    responses={200: "OK", 401: "Unauthorized"},
+)
+@api_view(["POST"])
+def login(request):
+    serializer = LoginSerializer(data=request.data)
 
-class LoginView(APIView):
-    authentication_classes = [TokenAuthentication]
+    if serializer.is_valid():
+        user = authenticate(
+            username=serializer.data.get("username"),
+            password=serializer.data.get("password"),
+        )
 
-    def post(self, request):
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            # get a profile pic if it exists
+            profile_pic = None
+            if user.profile_picture:
+                profile_pic = user.profile_pic.url
 
-        try:
-            # Your authentication logic here
-            user = authenticate(
-                username=request.data["username"], password=request.data["password"]
-            )
-            if user:
-                token, created = Token.objects.get_or_create(user=user)
-                # get a profile pic if it exists
-                profile_pic = None
-                if user.profile_picture:
-                    profile_pic = user.profile_pic.url
-
-                role = "Unknown"
-
-                if Tenant.objects.filter(user=user).exists():
-                    role = "Tenant"
-                elif Landlord.objects.filter(user=user).exists():
-                    role = "Landlord"
-                elif Agent.objects.filter(user=user).exists():
-                    role = "Agent"
-
-                return Response(
-                    {
-                        "token": token.key,
-                        # user metadata
-                        "username": user.username,
-                        "email": user.email,
-                        "first_name": user.first_name,
-                        "last_name": user.last_name,
-                        "website": user.website,
-                        "bio": user.bio,
-                        "location": user.location,
-                        "birth_date": user.birth_date,
-                        "phone_number": user.phone_number,
-                        "profile_picture": profile_pic,
-                        "kra_pin": user.kra_pin,
-                        "role": role,
-                    }
-                )
-            else:
-                return Response({"error": "Invalid credentials"}, status=400)
-        except KeyError as e:
             return Response(
                 {
-                    "error": "Please provide both username and password",
-                },
-                status=400,
+                    "token": token.key,
+                    # user metadata
+                    "username": user.username,
+                    "email": user.email,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                    "website": user.website,
+                    "bio": user.bio,
+                    "location": user.location,
+                    "birth_date": user.birth_date,
+                    "phone_number": user.phone_number,
+                    "profile_picture": profile_pic,
+                    "kra_pin": user.kra_pin,
+                    "role": user.role,
+                }
             )
+        else:
+            return Response({"error": "Invalid credentials"}, status=400)
+    else:
+
+        return Response(data={"error": "Invalid credentials"}, status=400)
