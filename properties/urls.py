5245881ff4fb698167f34dd3d4148df22a5b5054
Kagema Njoroge <reecejames934@gmail.com>
2024-06-30 22:41:46 +0300

    Add property unit urls

--- properties/urls.py
+++ properties/urls.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import HouseViewSet, PropertyViewSet, EntityPhotoViewSet
+from .views import HouseViewSet, PropertyViewSet, EntityPhotoViewSet, UnitViewSet
+
+app_name = "properties"
 
 router = DefaultRouter()
+router.register("units", UnitViewSet, basename="units")
 router.register("houses", HouseViewSet, basename="houses")
 router.register("", PropertyViewSet, basename="properties")
 router.register("photos", EntityPhotoViewSet, basename="photos")
