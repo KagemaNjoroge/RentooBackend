6ab6aeb07f9b04112bd7510f6fcc2cb643dcb867
Kagema Njoroge <reecejames934@gmail.com>
2024-06-30 22:29:50 +0300

    Add urls for the client facing app

---
+++ client/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+
+from .views import *
+from .property import *
+from .houses import *
+
+app_name = "client"
+
+urlpatterns = [
+    path("", index, name="index"),
+    path("login/", login, name="login"),
+    # property
+    path("property/", property, name="property"),
+    path("property/add/", new_property, name="new_property"),
+    path("property/<int:id>/", property_details, name="property_details"),
+    # houses
+    path("houses/add/", new_house, name="new_house"),
+    path("houses/<int:id>/", house_details, name="house_details"),
+    path("houses/", houses, name="houses"),
+    path("tenants/", tenants, name="tenants"),
+    path("agents/", agents, name="agents"),
+    path("leases/", leases, name="leases"),
+    path("reports/", reports, name="reports"),
+    path("communication/", communication, name="communication"),
+    path("landlords/", landlords, name="landlords"),
+    path("maintenance/", maintenance, name="maintenance"),
+    path("expenses/", expenses, name="expenses"),
+    path("purchases/", purchases, name="purchases"),
+    path("accounting/", accounting, name="accounting"),
+    path("users/", users, name="users"),
+    path("settings/", settings, name="settings"),
+    path("subscription/", subscription, name="subscription"),
+    path("cloud/", cloud, name="cloud"),
+]
