a807a0e05b5c92c46b6b478ee7b8581f45fcf811
Kagema Njoroge <reecejames934@gmail.com>
2024-07-12 23:30:34 +0300

    Refactor maintenance/urls.py to register maintenance endpoint with DefaultRouter

--- maintenance/urls.py
+++ maintenance/urls.py
@@ -3,6 +3,8 @@ from rest_framework.routers import DefaultRouter
 from .views import MaintenanceRequestViewSet
 
 router = DefaultRouter()
-router.register("", MaintenanceRequestViewSet, basename="maintenance_requests")
+router.register(
+    r"maintenance", MaintenanceRequestViewSet, basename="maintenance_requests"
+)
 
 urlpatterns = router.urls
