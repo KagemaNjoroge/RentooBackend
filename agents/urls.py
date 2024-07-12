3acf8cad9b4c38ddd04ee19762a53336cf447519
Kagema Njoroge <reecejames934@gmail.com>
2024-07-12 22:55:41 +0300

    Register agents endpoint

--- agents/urls.py
+++ agents/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from rest_framework.routers import DefaultRouter
+from .views import AgentsViewSet
+
+
+router = DefaultRouter()
+router.register("agents", AgentsViewSet, basename="agents")
+
+urlpatterns = router.urls
