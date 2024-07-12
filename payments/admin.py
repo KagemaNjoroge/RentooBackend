01823256778a11928d466e01d5daa792a942433e
Kagema Njoroge <reecejames934@gmail.com>
2024-07-12 23:31:08 +0300

    Refactor payments/admin.py to improve PaymentAdmin display and search functionality

---
+++ payments/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "lease",
+        "amount",
+        "payment_date",
+        "payment_method",
+        "created_at",
+        "updated_at",
+    )
+    search_fields = (
+        "lease__tenant__user__username",
+        "lease__tenant__user__first_name",
+        "lease__tenant__user__last_name",
+    )
+    list_filter = ("payment_date", "created_at", "updated_at")
