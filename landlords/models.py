05487cf03772844c5cf35dea516fd26c2f6d688b
Kagema Njoroge <reecejames934@gmail.com>
2024-07-12 23:27:31 +0300

    Refactor landlords/models.py to add PropertyCareTaker model

--- landlords/models.py
+++ landlords/models.py
@@ -1,10 +1,27 @@
 from django.db import models
-from authentication.models import CustomUser
+from properties.models import Property
 
 
-class Landlord(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+class PropertyCareTaker(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    property = models.ForeignKey(
+        Property,
+        on_delete=models.CASCADE,
+        related_name="caretakers",
+        blank=True,
+        null=True,
+    )
+    email = models.EmailField(blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to="property_care_takers/", blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
     class Meta:
-        verbose_name = 'Landlord'
-        verbose_name_plural = 'Landlords'
+        verbose_name = "Property Care Taker"
+        verbose_name_plural = "Property Care Takers"
