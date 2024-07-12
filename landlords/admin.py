3d23ab8b9cc6ce613b50931e16be8ac94fc4f224
Kagema Njoroge <reecejames934@gmail.com>
2024-07-12 23:24:08 +0300

    Refactor PropertyCareTakerAdmin in landlords/admin.py to display additional fields in the list view, enable searching by name, and add filters for created_at and updated_at

--- landlords/admin.py
+++ landlords/admin.py
@@ -1,14 +1,15 @@
 from django.contrib import admin
+from .models import PropertyCareTaker
 
-from .models import Landlord
 
-
-@admin.register(Landlord)
-class LandlordAdmin(admin.ModelAdmin):
-    list_display = ("user",)
-    search_fields = (
-        "user__username",
-        "user__first_name",
-        "user__last_name",
-        "user__email",
+@admin.register(PropertyCareTaker)
+class PropertyCareTakerAdmin(admin.ModelAdmin):
+    list_display = (
+        "first_name",
+        "last_name",
+        "phone_number",
+        "created_at",
+        "updated_at",
     )
+    search_fields = ("first_name", "last_name", "phone_number")
+    list_filter = ("created_at", "updated_at")
