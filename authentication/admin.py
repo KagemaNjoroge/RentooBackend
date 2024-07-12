148b30a54185e7081773ede8df4152fd0313d943
Kagema Njoroge <reecejames934@gmail.com>
2024-07-12 23:23:57 +0300

    Refactor CustomUserAdmin in authentication/admin.py for improved readability and maintainability

--- authentication/admin.py
+++ authentication/admin.py
@@ -4,20 +4,48 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'phone_number')
-    search_fields = ('username', 'email', )
-    list_filter = ('is_staff',)
+    list_display = ("username", "email", "phone_number")
+    search_fields = (
+        "username",
+        "email",
+    )
+    list_filter = ("is_staff",)
     fieldsets = (
-        (None, {'fields': ('username', 'email', )}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number', 'profile_picture')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (
+            None,
+            {
+                "fields": (
+                    "username",
+                    "email",
+                )
+            },
+        ),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name", "phone_number", "profile_picture")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email", "password1", "password2"),
+            },
+        ),
     )
-    ordering = ('username',)
-    filter_horizontal = ('groups', 'user_permissions')
+    ordering = ("username",)
+    filter_horizontal = ("groups", "user_permissions")
