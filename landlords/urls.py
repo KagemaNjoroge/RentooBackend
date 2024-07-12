509255c8249ac604dbea83cca0e67404f27b3119
Kagema Njoroge <reecejames934@gmail.com>
2024-07-12 23:27:47 +0300

    Refactor landlords/urls.py to use DefaultRouter for property-caretaker endpoint

--- landlords/urls.py
+++ landlords/urls.py
@@ -1,1 +1,15 @@
+from rest_framework.routers import DefaultRouter
+from .views import PropertyCareTakerViewSet
+
+router = DefaultRouter()
+router.register(
+    r"property-caretaker",
+    PropertyCareTakerViewSet,
+    basename="propertycaretaker",
+)
+
+
 urlpatterns = []
+
+
+urlpatterns += router.urls
