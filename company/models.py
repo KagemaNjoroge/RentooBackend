85bfa46cce69437c10b493748b937d52e6672c5a
Kagema Njoroge <reecejames934@gmail.com>
2024-07-14 23:15:26 +0300

    Refactor Company model in company app

--- company/models.py
+++ company/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(blank=True)
+    website = models.URLField(blank=True)
+    logo = models.ImageField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Company"
+        verbose_name_plural = "Companies"
+        ordering = ["name"]
