004ce7f114643bf1820ce5c3e6e98352bc39b142
Kagema Njoroge <reecejames934@gmail.com>
2024-07-14 23:16:43 +0300

    refactor: Remove unnecessary import in company/urls.py

--- company/urls.py
+++ company/urls.py
@@ -1,6 +1,5 @@
 from rest_framework.routers import DefaultRouter
 from .views import CompanyViewSet
-from rest_framework import status
 router = DefaultRouter()
 router.register("company", CompanyViewSet, basename="company")
 
