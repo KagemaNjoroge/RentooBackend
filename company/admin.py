bcfc43e88d00d14ba8f38f5ebae770be80e750f5
Kagema Njoroge <reecejames934@gmail.com>
2024-07-18 20:55:28 +0300

    refactor: Update CompanyAdmin in admin.py to include list_display for name and phone

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Company
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone')
+
+    def has_add_permission(self, request):
+        return Company.objects.count() < 1
