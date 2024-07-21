a9e6f3cab6bb80a5be8644855d99fe1ef6c9d83b
Kagema Njoroge <reecejames934@gmail.com>
2024-07-21 17:13:07 +0300

    refactor: Add VacationNotice model to 'vacate' app

--- vacate/models.py
+++ vacate/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from tenants.models import Lease
 
-# Create your models here.
+
+class VacationNotice(models.Model):
+    lease = models.ForeignKey(
+        Lease, on_delete=models.CASCADE, related_name="vacation_notices"
+    )
+    notice_date = models.DateTimeField(auto_now_add=True)
+    vacation_date = models.DateField()
+    reason = models.TextField()
+
+    def __str__(self):
+        return f"Vacation Notice: {self.lease.tenant} - {self.lease.house}"
+
+    class Meta:
+        ordering = ["-notice_date"]
+        verbose_name = "Vacation Notice"
+        verbose_name_plural = "Vacation Notices"
