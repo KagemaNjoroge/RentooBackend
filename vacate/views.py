ba5d44688dd148d0d4baedf2c167c069083b4b0f
Kagema Njoroge <reecejames934@gmail.com>
2024-07-21 17:13:31 +0300

    refactor: Add VacationNoticeViewSet to 'vacate' app

--- vacate/views.py
+++ vacate/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import VacationNotice
+from .serializers import VacationNoticeSerializer
 
-# Create your views here.
+
+class VacationNoticeViewSet(viewsets.ModelViewSet):
+    queryset = VacationNotice.objects.all()
+    serializer_class = VacationNoticeSerializer
