040171c90184670e8c0ebeaed657715a3affd17d
Kagema Njoroge <reecejames934@gmail.com>
2024-07-22 21:13:57 +0300

    Create payment method api routes

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet
+from .views import PaymentViewSet, PaymentMethodViewSet
 
 router = DefaultRouter()
 router.register(r"payments", PaymentViewSet, basename="payments")
+router.register(f"payment-methods", PaymentMethodViewSet, basename="payment_methods")
 
 
 urlpatterns = router.urls
