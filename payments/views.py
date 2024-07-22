5c9293593c98c15fd7fb4593026479aa8f9bab73
Kagema Njoroge <reecejames934@gmail.com>
2024-07-22 21:14:14 +0300

    Create payment method viewset

--- payments/views.py
+++ payments/views.py
@@ -1,7 +1,17 @@
 from rest_framework import viewsets
-from .serializers import Payment, PaymentSerializer
+from .serializers import (
+    Payment,
+    PaymentSerializer,
+    PaymentMethodSerializer,
+    PaymentMethod,
+)
 
 
 class PaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+
+
+class PaymentMethodViewSet(viewsets.ModelViewSet):
+    queryset = PaymentMethod.objects.all()
+    serializer_class = PaymentMethodSerializer
