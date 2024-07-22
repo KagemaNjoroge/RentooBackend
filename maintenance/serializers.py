3b5d3b2a600a82f8636b0960b3ba29fad2bdee34
Kagema Njoroge <reecejames934@gmail.com>
2024-07-22 23:03:55 +0300

    refactor: Add serializers for Maintainer and Maintenance models

--- maintenance/serializers.py
+++ maintenance/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from .models import MaintenanceRequest
+from .models import MaintenanceRequest, Maintainer, Maintenance
 
 
 class MaintenanceRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = MaintenanceRequest
+        fields = "__all__"
+
+
+class MaintainerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Maintainer
+        fields = "__all__"
+
+
+class MaintenanceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Maintenance
         fields = "__all__"
