01f24b9a5c0b86aacbf0893bb3d89992d39a5837
Kagema Njoroge <reecejames934@gmail.com>
2024-07-22 21:13:36 +0300

    Create payment method serializer

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Payment
+from .models import Payment, PaymentMethod
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
+        fields = "__all__"
+
+
+class PaymentMethodSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentMethod
         fields = "__all__"
