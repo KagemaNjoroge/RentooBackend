2fe6f6041d89f4e016523ccc8efc47a1de1cbff1
Kagema Njoroge <reecejames934@gmail.com>
2024-07-22 23:04:03 +0300

    refactor: Add Maintainer and Maintenance viewsets and serializers

--- maintenance/views.py
+++ maintenance/views.py
@@ -1,9 +1,23 @@
 from rest_framework import viewsets
 
-from .models import MaintenanceRequest
-from .serializers import MaintenanceRequestSerializer
+from .models import MaintenanceRequest, Maintainer, Maintenance
+from .serializers import (
+    MaintenanceRequestSerializer,
+    MaintainerSerializer,
+    MaintenanceSerializer,
+)
 
 
 class MaintenanceRequestViewSet(viewsets.ModelViewSet):
     queryset = MaintenanceRequest.objects.all()
     serializer_class = MaintenanceRequestSerializer
+
+
+class MaintainerViewSet(viewsets.ModelViewSet):
+    queryset = Maintainer.objects.all()
+    serializer_class = MaintainerSerializer
+
+
+class MaintenanceViewSet(viewsets.ModelViewSet):
+    queryset = Maintenance.objects.all()
+    serializer_class = MaintainerSerializer
