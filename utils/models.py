0daa4ab0ce38c9b8f5548fd837f10ac1b2842ac9
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 11:53:44 +0300

    feat: Add TemporaryFile model and migration for handling temporary image uploads

--- utils/models.py
+++ utils/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# TODO: create a cron job to delete temporary images every day at midnight
+class TemporaryFile(models.Model):
+    file = models.FileField(upload_to="temporary_files/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image.url
+
+    class Meta:
+        ordering = ["-created_at"]
