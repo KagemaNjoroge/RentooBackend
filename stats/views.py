6574a4608927e845a0c9d66bdaa3b4dfafa05062
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 17:34:31 +0300

    Create endpoint to get statistics such as number of houses, vacant houses. etc

--- stats/views.py
+++ stats/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from properties.models import Property, House
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(["GET"])
+def property_stats(request):
+    properties = Property.objects.all()
+    property_count = properties.count()
+    house_count = House.objects.all().count()
+    # unnoccupied houses
+    vacant_houses = House.objects.filter(is_occupied=False).count()
+    return Response(
+        {
+            "property_count": property_count,
+            "house_count": house_count,
+            "vacant_houses": vacant_houses,
+            "occupied_houses": house_count - vacant_houses,
+        }
+    )
