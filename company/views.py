8e96a9d8c931877a6d198f25840cbfbaf16aa3bb
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 11:31:43 +0300

    refactor: Add MailSettingsViewSet, MailSettingsSerializer, and MailSettings model for managing mail settings

--- company/views.py
+++ company/views.py
@@ -1,7 +1,17 @@
 from rest_framework import viewsets
-from .serializers import Company, CompanySerializer
+from .serializers import (
+    Company,
+    CompanySerializer,
+    MailSettings,
+    MailSettingsSerializer,
+)
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+
+class MailSettingsViewSet(viewsets.ModelViewSet):
+    queryset = MailSettings.objects.all()
+    serializer_class = MailSettingsSerializer
