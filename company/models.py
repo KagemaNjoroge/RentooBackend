9ff79a059040d0d2ef5ed90cfa875450095dd55b
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 11:31:28 +0300

    refactor: Add MailSettings model for managing mail settings

--- company/models.py
+++ company/models.py
@@ -19,3 +19,27 @@ class Company(models.Model):
         verbose_name = "Company"
         verbose_name_plural = "Companies"
         ordering = ["name"]
+
+
+class MailSettings(models.Model):
+    port = models.IntegerField()
+    smtp_server = models.CharField(max_length=30)
+    use_tls = models.BooleanField(default=False)
+    email = models.EmailField()
+    password = models.CharField(max_length=30)
+    name = models.CharField(max_length=50, default="default server")
+    description = models.TextField(blank=True, null=True)
+    is_default = models.BooleanField(default=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    # def save(self, *args, **kwargs) -> None:
+    #     # TODO: hash password
+    #     return super().save(args, kwargs)
+
+    class Meta:
+        verbose_name = "Mail Settings"
+        verbose_name_plural = "Mail Settings"
