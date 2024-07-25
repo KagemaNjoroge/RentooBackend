8752428f6554df6d7ec8d668fcecbbb0a7837689
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 11:31:33 +0300

    refactor: Add MailSettingsSerializer for managing mail settings

--- company/serializers.py
+++ company/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Company
+from .models import Company, MailSettings
 
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
+        fields = "__all__"
+
+
+class MailSettingsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MailSettings
         fields = "__all__"
