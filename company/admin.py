5f5d8da94ee49123719e87eab0208aeda0b7cbc8
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 11:31:22 +0300

    refactor: Add MailSettingsAdmin to handle mail settings in admin

--- company/admin.py
+++ company/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Company
+from .models import Company, MailSettings
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'phone')
+    list_display = ("name", "phone")
 
     def has_add_permission(self, request):
         return Company.objects.count() < 1
+
+
+@admin.register(MailSettings)
+class MailSettingsAdmin(admin.ModelAdmin):
+    list_display = ("name", "port", "smtp_server", "is_default")
+    search_fields = ("name", "description", "email")
