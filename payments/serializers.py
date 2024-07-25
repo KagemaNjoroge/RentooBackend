3e889b6349332c05801d9a249231300ddab716d2
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 14:03:06 +0300

    feat: Add MpesaPaymentSettingsSerializer for managing Mpesa payment settings

--- payments/serializers.py
+++ payments/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Payment, PaymentMethod
+from .models import Payment, PaymentMethod, MpesaPaymentSettings
 
 
 class PaymentSerializer(serializers.ModelSerializer):
@@ -11,4 +11,14 @@ class PaymentSerializer(serializers.ModelSerializer):
 class PaymentMethodSerializer(serializers.ModelSerializer):
     class Meta:
         model = PaymentMethod
+        fields = "__all__"
+
+
+class MpesaPaymentSettingsSerializer(serializers.ModelSerializer):
+    consumer_key = serializers.CharField(write_only=True)
+    consumer_secret = serializers.CharField(write_only=True)
+    pass_key = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = MpesaPaymentSettings
         fields = "__all__"
