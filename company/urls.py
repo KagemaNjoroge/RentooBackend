1d253cef811ee682d02332e8dde4cadf513bbf50
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 11:31:38 +0300

    refactor: Add MailSettingsViewSet for managing mail settings

--- company/urls.py
+++ company/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import CompanyViewSet
+from .views import CompanyViewSet, MailSettingsViewSet
 
 router = DefaultRouter()
-router.register("company", CompanyViewSet, basename="company")
+router.register(r"company", CompanyViewSet, basename="company")
+router.register(r"mail-settings", MailSettingsViewSet, basename="mail_settings")
 
 
 urlpatterns = router.urls
