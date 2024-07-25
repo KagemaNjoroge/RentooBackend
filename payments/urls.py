cfc9bd718410c72e99da4d09e59bd851076fec9c
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 14:02:55 +0300

    feat: Add MpesaPaymentSettingsViewSet for managing Mpesa payment settings

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet, PaymentMethodViewSet
+from .views import PaymentViewSet, PaymentMethodViewSet, MpesaPaymentSettingsViewSet
 
 router = DefaultRouter()
 router.register(r"payments", PaymentViewSet, basename="payments")
 router.register(f"payment-methods", PaymentMethodViewSet, basename="payment_methods")
+router.register(
+    r"mpesa-payment-settings",
+    MpesaPaymentSettingsViewSet,
+    basename="mpesa_payment_settings",
+)
 
 
 urlpatterns = router.urls
