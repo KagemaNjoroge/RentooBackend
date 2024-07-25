b21be884fb63b72591464d01b3c6b347a5ae8718
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 11:54:23 +0300

    feat: Add TemporaryFileViewSet for handling temporary file viewsets and routing

--- utils/views.py
+++ utils/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TemporaryFile
+from .serializers import TemporaryFileSerializer
 
-# Create your views here.
+
+class TemporaryFileViewSet(viewsets.ModelViewSet):
+    queryset = TemporaryFile.objects.all()
+    serializer_class = TemporaryFileSerializer
