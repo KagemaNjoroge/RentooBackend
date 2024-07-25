7f5d78793ac90b2f98b64af637f98406cfbb15ae
Kagema Njoroge <reecejames934@gmail.com>
2024-07-25 17:33:06 +0300

    Register TemporaryFile model in admin

--- utils/admin.py
+++ utils/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TemporaryFile
 
-# Register your models here.
+
+@admin.register(TemporaryFile)
+class TemporaryFileAdmin(admin.ModelAdmin):
+    list_display = ("created_at",)
