2b567a776d8bdcdd83d1ac9736e614a27f5f2d76
KagemaNjoroge <reecejames934@gmail.com>
2024-07-26 20:53:01 +0300

    feat: Add photos field to Unit and Property serializers

--- properties/serializers.py
+++ properties/serializers.py
@@ -2,25 +2,31 @@ from rest_framework import serializers
 from .models import Property, House, EntityPhoto, Unit
 
 
-class UnitSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Unit
-        fields = "__all__"
-
-
 class EntityPhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = EntityPhoto
         fields = "__all__"
 
 
+class UnitSerializer(serializers.ModelSerializer):
+    photos = EntityPhotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Unit
+        fields = "__all__"
+
+
 class PropertySerializer(serializers.ModelSerializer):
+    photos = EntityPhotoSerializer(many=True, read_only=True)
+
     class Meta:
         model = Property
         fields = "__all__"
 
 
 class HouseSerializer(serializers.ModelSerializer):
+    photos = EntityPhotoSerializer(many=True, read_only=True)
+
     class Meta:
         model = House
         fields = "__all__"
