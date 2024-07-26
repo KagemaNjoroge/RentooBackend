b325faf76940ba16efa04bbef960d53585365c2f
KagemaNjoroge <reecejames934@gmail.com>
2024-07-26 20:53:17 +0300

    feat: Update property_stats endpoint to include property houses

--- stats/views.py
+++ stats/views.py
@@ -1,10 +1,19 @@
 from properties.models import Property, House
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
+from django.shortcuts import get_object_or_404
+
+from properties.serializers import HouseSerializer
 
 
 @api_view(["GET"])
-def property_stats(request):
+def property_stats(request: Request):
+    """
+    Returns the number of properties,
+    houses, occupied and unoccupied houses
+    in the system
+    """
     properties = Property.objects.all()
     property_count = properties.count()
     house_count = House.objects.all().count()
@@ -18,3 +27,13 @@ def property_stats(request):
             "occupied_houses": house_count - vacant_houses,
         }
     )
+
+
+@api_view(["GET"])
+def property_houses(request: Request, id: int):
+    property = get_object_or_404(Property, id=id)
+    houses = House.objects.filter(property=property)
+    serializer = HouseSerializer(houses, many=True)
+    count = houses.count()
+    
+    return Response(serializer.data)
