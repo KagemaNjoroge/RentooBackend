cd256b6ae682595b805e58a9b50f972b432d620d
KagemaNjoroge <reecejames934@gmail.com>
2024-07-26 20:53:09 +0300

    Refactor stats app URLs to include property houses endpoint

--- stats/urls.py
+++ stats/urls.py
@@ -1,7 +1,8 @@
-from .views import property_stats
+from .views import property_stats, property_houses
 from django.urls import path
 
 
 urlpatterns = [
     path("property-stats/", property_stats, name="property_stats"),
+    path("property/<int:id>/", property_houses),
 ]
