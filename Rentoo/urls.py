345808fc28b96cdf5d8c909210f78cafc7baa2ec
KagemaNjoroge <reecejames934@gmail.com>
2024-07-30 20:54:24 +0300

    feat: Enable JWT token authentication for API endpoints

--- Rentoo/urls.py
+++ Rentoo/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
 from django.conf.urls.static import static
 from Rentoo import settings
 
@@ -67,16 +68,16 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     # jwt token
-    path(
-        "api/token/",
-        TokenObtainPairView.as_view(),
-        name="token_obtain_pair",
-    ),
-    path(
-        "api/token/refresh/",
-        TokenRefreshView.as_view(),
-        name="token_refresh",
-    ),
+    # path(
+    #     "api/token/",
+    #     TokenObtainPairView.as_view(),
+    #     name="token_obtain_pair",
+    # ),
+    # path(
+    #     "api/token/refresh/",
+    #     TokenRefreshView.as_view(),
+    #     name="token_refresh",
+    # ),
     # client
     path(
         "client/",
